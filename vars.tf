bbe81494b727ee53c85bc9ad9db005482a80c3da
Stephen Archer <s.archer@f5.com>
2021-03-31 17:13:54 +0100

    updated vars definition

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "f5_ui" {}
+
+variable "f5_username" {}
+
+variable "f5_password" {}
+
+variable "as3tenant" {}
+
+variable "as3app" {}
+
+variable "as3vip" {}
+
+variable "app_definition" {}
